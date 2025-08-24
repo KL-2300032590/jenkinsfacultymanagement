261a6812d1d0c43adf4ccfcb314a6297f72dbf9c
PARDHASARADHI REDDY <2300032590@kluniversity.in>
2025-08-25 00:44:54 +0530

    Update api.js

--- facultyreactapp/src/api/api.js
+++ facultyreactapp/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:1985/klu/faculty"
+  baseURL: "http://localhost:1985/Facultymanagement/klu/faculty"
 });
 
 export default api;
