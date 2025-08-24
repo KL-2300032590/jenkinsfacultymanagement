d74e6c40de0834e25cb918700f4a2cd2f851519b
PARDHASARADHI REDDY <2300032590@kluniversity.in>
2025-08-25 00:41:47 +0530

    Update FacultyDemoApplication.java

--- FacultyDemo/src/main/java/com/faculty/fsd/FacultyDemoApplication.java
+++ FacultyDemo/src/main/java/com/faculty/fsd/FacultyDemoApplication.java
@@ -2,13 +2,19 @@ package com.faculty.fsd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FacultyDemoApplication {
+public class FacultyDemoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FacultyDemoApplication.class, args);
-		System.out.println("Project is Running....");
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FacultyDemoApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(FacultyDemoApplication.class, args);
+        System.out.println("Project is Running....");
+    }
 }
